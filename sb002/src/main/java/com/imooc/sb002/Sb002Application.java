418b2976aba11af0c6e4fffc48d7339996c3711e
fzj <fengzongjian@yeah.net>
2018-04-12 15:08:52 +0800

    sb002，Spring Boot 测试练习

---
+++ sb002/src/main/java/com/imooc/sb002/Sb002Application.java
@@ -0,0 +1,12 @@
+package com.imooc.sb002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb002Application.class, args);
+	}
+}
